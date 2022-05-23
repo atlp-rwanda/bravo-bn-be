77ab64045e5f23d0a57ecc34fa0375509ac04375
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-05-23 17:19:29 +0200

    add a route for testing deploy link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("app listening on port 3000");
+app.get("/welcome", (req,res)=>{
+    res.json({message: "Welcome to barefoot!"})
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`listening on port ${process.env.PORT}`)
 })
