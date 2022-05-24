904dd011e4c53c6cf159f5d038d4a07d7496879b
Rose <mwisemarierose@gmail.com>
2022-05-24 09:24:03 +0200

    chore(update):update-project-environment
    update empty express boilerplate
    WIP(#182201658)

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import router from './routes/index';
+import morgan from 'morgan';
+import '@babel/polyfill';
+
 const app = express();
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.json()); 
-app.use(router);
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/public`));
 
-
+app.get("/",(req,res,next)=>{
+    res.status(200).json({message:"welcome"});
+})
 
 export default app;
