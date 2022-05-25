f1f102a053b830d915f3cbf3727cbe5f7244c695
Placide123 <placidetwiringiyimana@outlook.com>
2022-05-25 16:15:49 +0200

    chore(seed):change-seed
    change seeder into ES6
    chance migration into ES6
    
    [work in progress #182201659]

--- src/seeders/20220523193745-demo-user.js
+++ src/seeders/20220523193745-demo-user.js
@@ -1,18 +1,14 @@
 'use strict';
 
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('users', [{
-      username: 'John Doe',
-      password: "123456",
-      fullname: "placide twiringiyimana",
-      role:"client",
-      email:"twiringiyimanat@gmail.com"
-
-    }], {});
-  },
-
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('users', null, {});
-  }
-};
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.bulkInsert('users', [{
+    username: 'John Doe1',
+    password: "123456",
+    fullname: "placide twiringiyimana",
+    role: "client",
+    email: "twiringiyimanat@gmail.com"
+  }], {});
+}
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.bulkDelete('users', null, {});
+}
