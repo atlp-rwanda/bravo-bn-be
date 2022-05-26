44e6ddad08278597186e115ef8be75cbc706a25a
Alain Tresor <55650162+alaintresor@users.noreply.github.com>
2022-05-26 15:33:20 +0200

    chore(doc):Setup API documentation using swagger (#4)
    * chore(doc):Setup API documentation using swagger

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import express from 'express';
+const app = express();
+import docsRouter from './Documentation/index.doc';
+import testSwaggerRouter from './routers/testSwaggerRouter';
 import dotenv from 'dotenv'
-
 dotenv.config();
 
-const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req,res) => {
     res.json({message: "Welcome to barefoot!"})
 });
+
+app.use('/api/testSwagger', testSwaggerRouter);
+app.use('/api/docs',docsRouter)
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`listening on port ${process.env.PORT}`)
