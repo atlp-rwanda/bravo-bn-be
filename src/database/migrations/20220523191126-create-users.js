88d86a599bee5640f3b1c39f8d30fc60ce9a6e5b
alaintresor <tresoralain35@gmail.com>
2022-05-31 17:56:03 +0200

    final commit

--- src/database/migrations/20220523191126-create-users.js
+++ src/database/migrations/20220523191126-create-users.js
@@ -1,44 +1,83 @@
 'use strict';
-export async function up(queryInterface, Sequelize) {
-  await queryInterface.createTable('users', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
-    },
-    fullname: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    username: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    role: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    password: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    createdAt: {
-      allowNull: true,
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.fn("NOW")
-    },
-    updatedAt: {
-      allowNull: true,
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.fn("NOW")
-    }
-  });
-}
-export async function down(queryInterface, Sequelize) {
-  await queryInterface.dropTable('users');
-}
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      phoneNumber: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      passwordChangedAt: {
+        type: Sequelize.DATE
+      },
+      passwordResetExpires: {
+        type: Sequelize.DATE
+      },
+      passwordResetToken: {
+        type: Sequelize.STRING
+      },
+      socialMediaId:{
+        type: Sequelize.STRING
+      },
+      provider:{
+        type: Sequelize.STRING
+      },
+      isVerified:{
+        type: Sequelize.BOOLEAN
+      },
+      gender:{
+        type: Sequelize.STRING
+      },
+      preferredLanguage:{
+        type: Sequelize.STRING
+      },
+      preferredCurrency:{
+        type: Sequelize.STRING
+      },
+      department:{
+        type: Sequelize.STRING
+      },
+      lineManager:{
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.ENUM,
+        values: ['super user', 'super admin', 'travel admin', 'travel team member',
+          'manager', 'requester', 'accommodation supplier']
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
