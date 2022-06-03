046170543386e4bfd4d1c147129c9b127f2b0944
alaintresor <tresoralain35@gmail.com>
2022-06-03 16:02:34 +0200

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import 'dotenv/config';
 import app from './app';
-import dotenv from 'dotenv';
 
-dotenv.config(".env");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app is listening on port ${PORT}`);
+});
 
-const port = process.env.PORT;
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
+
+
