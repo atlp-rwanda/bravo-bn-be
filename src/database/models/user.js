84d2aa3fb02d1fd4c3809eb0220d1d8ebd2aa15b
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2022-06-04 10:54:56 +0200

    #182201714 Setup docker for backend application  (#8)
    * chore(docker): setup docker for back-end application

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -25,6 +25,9 @@ export default (sequelize, DataTypes) => {
     phoneNumber: DataTypes.STRING,
     passwordChangedAt: DataTypes.DATE,
     passwordResetExpires: DataTypes.DATE,
+    phoneNumber: DataTypes.STRING,
+    passwordChangedAt: DataTypes.DATE,
+    passwordResetExpires: DataTypes.DATE, 
     passwordResetToken: DataTypes.STRING,
     role: DataTypes.ENUM(
         'manager', 
