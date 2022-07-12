fad9ed092cb59227a1da4bfaeb9a91ee7321e46c
Rose <mwisemarierose@gmail.com>
2022-07-12 21:38:51 +0200

    rate accomodation

---
+++ src/database/models/rates.js
@@ -0,0 +1,30 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class rates extends Model {
+    static associate({ accomodation }) {
+      this.belongsTo(accomodation, {
+        foreignKey: 'accomodationId',
+        as: 'accomodation',
+      });
+    }
+    toJSON() {
+      return {
+        ...this.get(),
+        id: undefined,
+        accomodationId: undefined,
+      };
+    }
+  }
+  rates.init(
+    {
+      rating: DataTypes.INTEGER,
+      accomodationId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'rates',
+    },
+  );
+  return rates;
+};
