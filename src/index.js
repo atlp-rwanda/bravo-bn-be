4f8a14b02a36e039c042cc95c04a0d4889af3475
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-07-18 17:33:55 +0200

    integrate socket.io and create an event

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import 'dotenv/config';
 import app from './app';
+import http from 'http';
+import socketio from 'socket.io';
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  },
+});
+
+io.on('connect', (socket) => {
+  console.log(`a user connected, socket id: ${socket.id}`);
+
+  socket.on('test-event', () => {
+    console.log('An event just occured!');
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`app is listening on port ${PORT}`);
 });
