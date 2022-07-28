9f491b0a5d3f47617d4400def32f16f69f914d59
Rose <mwisemarierose@gmail.com>
2022-07-28 17:01:13 +0200

    ft(rate):rate accomodation centre
    Users should be able to rate accommodation facilities in the centres that they have travelled to

--- src/controllers/rates.controller.js
+++ src/controllers/rates.controller.js
@@ -92,7 +92,7 @@ export const createRate = catchAsync(async (req, res, next) => {
         },
       },
     );
-    return res.status(201).json({
+    return res.status(200).json({
       message: 'rates updated',
       data: rates,
     });
